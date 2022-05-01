107290897ce84c2837e9f500a1c9d5e366557d81
Giuseppe Desiato <giodesiato@gmail.com>
2022-04-30 23:17:47 -0400

    completed Exercise_04

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,26 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        byte byteVal = 58;
+        System.out.println("byte byteVal is: " + byteVal);
+
+        short shortVal = 30000;
+        System.out.println("short shortVal is: " + shortVal);
+
+        long longVal = 6723372075807L;
+        System.out.println("long longVal is: " + longVal);
+
+        float floatVal = 67876.76790f;
+        System.out.println("long floatVal is: " + floatVal);
+
+        double doubleVal = 2341.561389;
+        System.out.println("double doubleVal is: " + doubleVal);
+
+        char charVal = 'c';
+        System.out.println("char charVal is: " + charVal);
+
+        boolean isReal = true;
+        System.out.println("boolean isReal is: " + isReal);
 
     }
 
