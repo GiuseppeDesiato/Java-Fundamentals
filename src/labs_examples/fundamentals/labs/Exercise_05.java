613049b170eaf300b4dd6d04d7051e1cd4c56e86
Giuseppe Desiato <giodesiato@gmail.com>
2022-04-30 23:19:05 -0400

    completed Exercise_05

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,30 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int lengthStr = str.length();
+        System.out.println(lengthStr);
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean isEqual = str.equals(str2);
+        System.out.println(isEqual);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String str3 = str + str2;
+        System.out.println(str3);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        int charIndex = str2.indexOf("h");
+        System.out.println(charIndex);
+
+        String str4 = str.replace("l", "p");
+        System.out.println(str4);
+
+        System.out.println(str.substring(1,3));
+        System.out.println(str4.contains("pp"));
+
+
 
     }
 
