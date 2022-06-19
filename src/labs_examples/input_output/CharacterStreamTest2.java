61d095e3717c94529dc11195d6ea423d7de9f23b
Giuseppe Desiato <giodesiato@gmail.com>
2022-06-19 10:42:57 -0400

    character stream

---
+++ src/labs_examples/input_output/CharacterStreamTest2.java
@@ -0,0 +1,40 @@
+package labs_examples.input_output;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CharacterStreamTest2 {
+
+    public static void printFile(String filePath) throws IOException {
+
+        // initialize FileReader "fr" to null
+        FileReader fr = null;
+
+        try {
+            /*  2. Fully initialize Reader "fr" with "filePath" variable in Try
+                statement in case files don't exist
+                or can't be accessed. Remember to pass the
+                filePath as a parameter */
+
+            fr = new FileReader(filePath);
+
+            int c = fr.read();
+
+            // 3. While fr has more characters to read
+            while (c != -1) {
+                // Print the character to the console
+                // don't forget to cast the int to a char
+                System.out.print((char) c);
+                c = fr.read();
+            }
+        } catch (IOException exc) {
+            System.out.println("An error occurred: " + exc.getMessage());
+        } finally {
+
+            if (fr != null) {
+                // 4. Close connections
+                fr.close();
+            }
+        }
+    }
+}
