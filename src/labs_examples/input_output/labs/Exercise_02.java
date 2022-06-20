5bd77d2873b1b3960a3b5e23a250acb1575c9981
Giuseppe Desiato <giodesiato@gmail.com>
2022-06-20 19:29:55 -0400

    IO exercise_02

--- src/labs_examples/input_output/labs/Exercise_02.java
+++ src/labs_examples/input_output/labs/Exercise_02.java
@@ -1,5 +1,8 @@
 package labs_examples.input_output.labs;
 
+
+import java.io.*;
+
 /**
  * Input/Output Exercise 2: File encryption
  *
@@ -11,4 +14,42 @@ package labs_examples.input_output.labs;
  *      print out the unencrypted version. Does it match the original file?
  *
  */
+
+class Exercise_2_no_Buffer_Reader {
+    public static void main(String[] args) throws IOException {
+
+        FileReader inputStream = null;
+        //BufferedReader bufferedReader= null;
+        FileWriter outputStream = null;
+
+        try {
+            inputStream = new FileReader("src/labs_examples/input_output/labs/exercise_1.txt");
+            //bufferedReader = new BufferedReader(inputStream);
+            outputStream = new FileWriter("src/labs_examples/input_output/labs/exercise_2_encr.txt");
+
+            int i;
+
+            while ((i = inputStream.read()) != -1) {
+                if (i == 'a'){
+                    i = '-';
+                } else if (i == 'e'){
+                    i = '~';
+                }
+                outputStream.write(i);
+                System.out.print((char) i);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // close connections
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+}
 
