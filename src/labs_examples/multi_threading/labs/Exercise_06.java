f3584f0a274d3e6a436bf0a5c3962cf12d1fe59e
Giuseppe Desiato <giodesiato@gmail.com>
2022-07-06 10:18:35 -0400

    threads_incr_nums

--- src/labs_examples/multi_threading/labs/Exercise_06.java
+++ src/labs_examples/multi_threading/labs/Exercise_06.java
@@ -9,8 +9,63 @@ package labs_examples.multi_threading.labs;
 
 class Exercise_06 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Print100 obj = new Print100();
 
+        PrintThread_01 printThread_01 = new PrintThread_01(obj);
+        PrintThread_02 printThread_02 = new PrintThread_02(obj);
+
+        printThread_01.start();
+        printThread_02.start();
+
+        printThread_01.join();
+        printThread_02.join();
+
+        }
+    }
+
+class Print100{
+
+    public void print100(){
+        // synchronized block
+        synchronized(this){
+
+            for(int i = 1; i <= 100; i++){
+                System.out.println(i);
+                try{
+                    Thread.sleep(100);
+                }catch(Exception e){
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+}
+
+class PrintThread_01 extends Thread {
+    Print100 p100;
+
+    PrintThread_01(Print100 p100){
+        this.p100 = p100;
+    }
+
+    @Override
+    public void run() {
+        p100.print100();
+
+    }
+}
+
+class PrintThread_02 extends Thread {
+    Print100 p100;
+
+    PrintThread_02(Print100 p100){
+        this.p100 = p100;
+    }
+
+    @Override
+    public void run() {
+        p100.print100();
 
     }
 }
--- src/labs_examples/multi_threading/labs/FoodProcess.java
+++ src/labs_examples/multi_threading/labs/FoodProcess.java
@@ -4,22 +4,18 @@ import java.util.Scanner;
 
 public class FoodProcess {
 
-    public void buy() throws InterruptedException{
+    public synchronized void buy() throws InterruptedException{
         //this is going to run first
-        synchronized (this){
             System.out.println("Buy thread running");
             wait();
             System.out.println("Food consumed.");
-        }
 
     }
 
-    public void eat() throws InterruptedException{
+    public synchronized void eat() throws InterruptedException{
         Thread.sleep(500);
         //due to the .sleep this thread is going to run after the first one.
-        synchronized (this){
             System.out.println("Eat thread running");
-            notify();
-        }
+            notifyAll();
     }
 }
