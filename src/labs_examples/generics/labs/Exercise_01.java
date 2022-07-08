9476cf5835ec420f386fb0768fbf7e9b1e438944
Giuseppe Desiato <giodesiato@gmail.com>
2022-07-08 18:57:07 -0400

    generics

--- src/labs_examples/generics/labs/Exercise_01.java
+++ src/labs_examples/generics/labs/Exercise_01.java
@@ -8,3 +8,48 @@ package labs_examples.generics.labs;
  *          dynamic usage.
  */
 
+class Exercise_01{
+    public static void main(String[] args) {
+
+        TestGen <String, String> gen1 = new TestGen("Hello", "World");
+        TestGen <Integer, Double> gen2 = new TestGen(123, 24.5);
+        TestGen <String, Double> gen3 = new TestGen("I am", 5.9);
+
+        gen1.print();
+        gen2.print();
+        gen3.print();
+
+    }
+}
+
+
+class TestGen<T, V>{
+
+    private T val1;
+    private V val2;
+
+    public TestGen(T val1, V val2) {
+        this.val1 = val1;
+        this.val2 = val2;
+    }
+
+    public void print() {
+        System.out.println(val1 + " " + val2);
+    }
+
+    public T getVal1() {
+        return val1;
+    }
+
+    public void setVal1(T val1) {
+        this.val1 = val1;
+    }
+
+    public V getVal2() {
+        return val2;
+    }
+
+    public void setVal2(V val2) {
+        this.val2 = val2;
+    }
+}
