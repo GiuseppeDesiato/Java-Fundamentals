8ff8072eb7d6768570a2f94053abf2269db72930
Giuseppe Desiato <giodesiato@gmail.com>
2022-07-11 09:40:24 -0400

    update exercise 3

--- src/labs_examples/generics/labs/Exercise_03.java
+++ src/labs_examples/generics/labs/Exercise_03.java
@@ -22,36 +22,61 @@ class Exercise_03 {
 
         Operations op = new Operations();
 
+        //1)
         double sum = op.sumDouble(89.32, 78.19);
         System.out.println(sum);
 
+        //2)
         String[] strArray = {"dog", "anna", "otto", "cat", "gigi"};
         int count = op.countElem(strArray);
         System.out.println(count);
 
+        //3)
+        op.switchPos(strArray, 1,4);
+        op.printArray(strArray);
     }
 }
 
 class Operations{
 
+    //1)
     public static <T extends Number, V extends Number> double sumDouble(T a, V b){
         return a.doubleValue() + b.doubleValue();
     }
 
+    //2) modify in order to become a generic method
     public int countElem(String[] strList){
-        String str = "";
 
         int countPal = 0;
 
+        boolean flag = true;
+
         for(int i = 0; i < strList.length; i++) {
-            for(int j = 0; j < str.length(); j++){
-                if(str.charAt(j) != (str.charAt(str.length() - 1 - j))) {
-                    countPal = countPal;
+            for(int j = 0; j < strList[i].length()/2; j++){
+                if(strList[i].charAt(j) != (strList[i].charAt(strList[i].length() - 1 - j))) {
+                    flag = false;
                 } else {
-                    countPal++;
+                    flag = true;
                 }
+            }
+            if (flag){
+                countPal++;
             }
         }
         return countPal;
     }
+
+    //3)
+    public <T> void switchPos(T[] arr, int i, int j){
+        T temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public <T> void printArray(T[] arr){
+        for(int i =0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
 }
