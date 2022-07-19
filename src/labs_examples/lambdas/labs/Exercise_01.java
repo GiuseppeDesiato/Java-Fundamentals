4e042b4c55924d715b0bdd3063f406f816a9cf85
Giuseppe Desiato <giodesiato@gmail.com>
2022-07-19 09:50:11 -0400

    lambda labs

--- src/labs_examples/lambdas/labs/Exercise_01.java
+++ src/labs_examples/lambdas/labs/Exercise_01.java
@@ -1,5 +1,8 @@
 package labs_examples.lambdas.labs;
 
+import java.util.function.BiFunction;
+import java.util.function.DoubleFunction;
+
 /**
  * Lambdas Exercise 1:
  *
@@ -23,3 +26,91 @@ package labs_examples.lambdas.labs;
  *
  *
  */
+
+public class Exercise_01 {
+
+    public static void main(String[] args) {
+
+        // 2)
+        NoParam noParam = () -> {
+            System.out.println("Implementation of an abstract method with no parameters");
+        };
+        noParam.method1();
+
+        // Anonymous inner class
+        NoParam noParamAnon = new NoParam() {
+            @Override
+            public void method1() {
+                System.out.println("Example using an anonymous inner class");
+            }
+        };
+        noParamAnon.method1();
+
+        // 4)
+        OneParam oneParam = (double a) -> {
+            return a * a;
+        };
+        double x = oneParam.method2(5.2);
+        System.out.println("Implementation of a one-parameter interface using lambda: " + x);
+
+        OneParam oneParam2 = new OneParam() {
+            @Override
+            public double method2(double a) {
+                return a + a;
+            }
+        };
+        double y = oneParam2.method2(6.5);
+        System.out.println("Implementation of a one-parameter interface using an anonymous inner class: " + x);
+
+
+        // 6)
+        TwoParam twoParam = (int a, int b) -> {
+            return a / b;
+        };
+        int q = twoParam.method3(6, 3);
+        System.out.println("Implementation of a two-parameter interface using lambda: " + q);
+
+        TwoParam twoParam2 = new TwoParam() {
+            @Override
+            public int method3(int a, int b) {
+                return a * b;
+            }
+        };
+        int w = twoParam2.method3(6, 5);
+        System.out.println("Implementation of a two-parameter interface using an anonymous inner class: " + w);
+
+        // 7) use of built-in functional interface
+
+        BiFunction <Integer, Integer, Integer> biFunction = (Integer a, Integer b) -> {
+            return a - b;
+        };
+        int r = biFunction.apply(5, 3);
+        System.out.println("BiFunction : " + r);
+
+
+        DoubleFunction <Double> doubleFunction = (double a) -> {
+            return Math.pow(a, a);
+        };
+        double t = doubleFunction.apply(6);
+        System.out.println("DoubleFunction: " + t);
+
+    }
+}
+
+
+// 1)
+interface NoParam {
+    public void method1();
+}
+
+// 3)
+
+interface OneParam {
+    public double method2 (double a);
+}
+
+//
+
+interface TwoParam {
+    public int method3 (int a, int b);
+}
--- src/labs_examples/lambdas/labs/Exercise_02.java
+++ src/labs_examples/lambdas/labs/Exercise_02.java
@@ -1,5 +1,6 @@
 package labs_examples.lambdas.labs;
 
+import java.util.function.*;
 /**
  * Lambdas Exercise 2:
  *
@@ -7,3 +8,88 @@ package labs_examples.lambdas.labs;
  *      10 of them! You can do it! You'll know so much about them when you're done!
  *
  */
+
+public class Exercise_02 {
+
+    public static void main(String[] args) {
+
+        //1. IntFunction
+        IntFunction <Integer> intFunction = (a) -> {
+            if (a > 10)
+                return a + a;
+            else
+                return a * a;
+        };
+        int q = intFunction.apply(12);
+        System.out.println(q);
+
+
+        //2. BiPredicate
+        BiPredicate <Double, Double> biPredicate = (a, b) -> {
+            if (a > b)
+                return true;
+            else
+                return false;
+        };
+        boolean b = biPredicate.test(678.3, 567.3);
+        System.out.println(b);
+
+
+        //3. BinaryOperator
+        BinaryOperator <Double> binaryOp = (x, y) -> {
+          return Math.pow(x,y);
+        };
+        double s = binaryOp.apply(3.3, 2.2);
+        System.out.println(s);
+
+
+        //4. Consumer
+        Consumer<String> consumer = (str) -> {
+            System.out.println(str);
+        };
+        consumer.accept("Hello!");
+
+
+        //5. Function
+        Function<String, Integer> function = (x) -> {
+            return x.length();
+        };
+        int l = function.apply("Gigio");
+        System.out.println(l);
+
+
+        //6. IntBinaryOperator
+        IntBinaryOperator intBinary = (c, d) -> {
+            return (c * d) + c;
+        };
+        int w = intBinary.applyAsInt(23,34);
+        System.out.println(w);
+
+
+        //7. IntToDoubleFunction. Takes an int and give a double
+        IntToDoubleFunction funct = (a) -> {
+            return a;
+        };
+        System.out.println(funct.applyAsDouble(15));
+
+
+        //8. DoubleBinaryOperator
+        DoubleBinaryOperator doubleBinary = (k, j) -> {
+            return k * j;
+        };
+        System.out.println(doubleBinary.applyAsDouble(78,90));
+
+
+        //9. BiConsumer
+        BiConsumer <Integer, Double> biConsumer = (v, n) -> {
+            System.out.println(v * n);
+        };
+        biConsumer.accept(45, 98.6);
+
+
+        //10. BooleanSupplier
+        BooleanSupplier booleanS = () -> true;
+        System.out.println(booleanS.getAsBoolean());
+
+    }
+}
