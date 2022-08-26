9924d52e7e4053db154cd86cba56d877e3384fda
Giuseppe Desiato <giodesiato@gmail.com>
2022-08-26 08:47:22 -0400

    formatting

---
+++ src/labs_examples/formatting/example_1.java
@@ -0,0 +1,24 @@
+package labs_examples.formatting;
+
+public class example_1 {
+
+    public static void main(String[] args) {
+
+        String example1 = String.format(">%d<", 35);
+        System.out.println(example1);
+
+        String example2 = String.format(">%7d<", 35);
+        System.out.println(example2);
+
+        String example3 = String.format(">%07d<", 35);
+        System.out.println(example3);
+
+        String example4 = String.format(">%-7d<", 35);
+        System.out.println(example4);
+
+        String example5 = String.format(">%7.3f<",35.0);
+        System.out.println(example5);
+
+    }
+
+}
