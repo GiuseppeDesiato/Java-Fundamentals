fa878ec0562544c01b89b215f0d1152723b82bfc
Giuseppe Desiato <giodesiato@gmail.com>
2022-08-27 08:36:29 -0400

    var

---
+++ src/labs_examples/variables/var_content.java
@@ -0,0 +1,23 @@
+package labs_examples.variables;
+
+public class var_content {
+
+    public static void main(String[] args) {
+
+        // Position 1
+        short x, y;
+        // Position 2
+        x = 1234;
+        // Position 3
+        y = 99;
+        // Position 4
+        short z = x;
+        // Position 5
+        y = x;
+        // Position 6
+
+        System.out.println("x is :" + x);
+        System.out.println("y is :" + y);
+
+    }
+}
