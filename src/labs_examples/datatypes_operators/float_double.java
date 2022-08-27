a9f2bb54e1f209a83df90821cf63ff41bd024398
Giuseppe Desiato <giodesiato@gmail.com>
2022-08-27 09:03:42 -0400

    float_double

---
+++ src/labs_examples/datatypes_operators/float_double.java
@@ -0,0 +1,15 @@
+package labs_examples.datatypes_operators;
+
+public class float_double {
+
+    public static void main(String[] args) {
+
+        float x = 1234.123F; // the float number needs and F at the end of it,
+                             // otherwise the editor gives an error. because Java assumes that that number is a double
+        double y = 1234.123;
+
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
