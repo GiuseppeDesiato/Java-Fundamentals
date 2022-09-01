2403b758eb2a2de3e22d900a5a979a90f0bcc3ef
Giuseppe Desiato <giodesiato@gmail.com>
2022-09-01 17:01:04 -0400

    java to mysql

--- src/mysql/labs/Flight.java
+++ src/mysql/labs/Flight.java
@@ -3,9 +3,20 @@ package mysql.labs;
 public class Flight {
 
     String company_name = null;
+    int id = 0;
 
     public String createFlight(String company_name){
         String sql = "INSERT INTO AirTravel.planes (company_name) VALUES ( '" + company_name + "')";
+        return sql;
+    }
+
+    public String deleteFlight(int id){
+        String sql = "DELETE FROM AirTravel.planes WHERE (`id` = " + "'" + id + "'" + ")";
+        return sql;
+    }
+
+    public String updateFlight(String company_name, int id){
+        String sql = "UPDATE AirTravel.passengers SET `company_name` = '" + company_name  + " WHERE (`id` = " + "'" + id + "'" + ")";
         return sql;
     }
 
